7a7870e2bddd2ca0dd0f5d0a4861cae64b011e75
Avi-Cohen <aviko.cohen@gmail.com>
2020-09-01 09:33:18 +0300

    added try and catch blocks to the async function , and log the path of the files, trying to deal with the .DS_store file

--- backend/pdfParsing/pdfParse.js
+++ backend/pdfParsing/pdfParse.js
@@ -5,8 +5,9 @@ const pdfParse = require('pdf-parse');
 const files = fs.readdirSync('/Users/avicohen/vs-sandbox/itst/backend/pdfParsing/allPdfResults');
 
 files.forEach(file => {
-    console.log(__dirname + '/allPdfResults/' + file)
-    const dataBuffer = fs.readFileSync(__dirname + '/allPdfResults/' + file);
+    let path = __dirname + '/allPdfResults/' + file;
+    console.log(path);
+    const dataBuffer = fs.readFileSync(path);
     try {
         parseResults(dataBuffer);
     } catch (error) {
@@ -16,37 +17,41 @@ files.forEach(file => {
 })
 
 async function parseResults(buffer) {
-    const data = await pdfParse(buffer);
-    const rows = data.text.split('\n');
-    const firstPlaceIndex = rows.findIndex((row, index) => row === '1' && rows[index + 5] === '2') // find the index of the first result
-    const parsedResults = [];
-    for (let i = firstPlaceIndex; i < rows.length; i += 5) {
-        parsedResults.push({
-            name: reverseString(rows[i + 1]),
-            yearOfBirth: rows[i + 2],
-            club: reverseString(rows[i + 3]),
-            heat: rows[i + 4][0],
-            lane: rows[i + 4][1],
-            time: (() => {
-                if (rows[i + 4][2] !== 'D' && rows[i + 4][2] !== 'N') {
-                    return rows[i + 4].slice(2, 10);
-                } else {
-                    return rows[i + 4].slice(2, 4);
-                }
-            })(),
+    try {
+        const data = await pdfParse(buffer);
+        const rows = data.text.split('\n');
+        const firstPlaceIndex = rows.findIndex((row, index) => row === '1' && rows[index + 5] === '2') // find the index of the first result
+        const parsedResults = [];
+        for (let i = firstPlaceIndex; i < rows.length; i += 5) {
+            parsedResults.push({
+                name: reverseString(rows[i + 1]),
+                yearOfBirth: rows[i + 2],
+                club: reverseString(rows[i + 3]),
+                heat: rows[i + 4][0],
+                lane: rows[i + 4][1],
+                time: (() => {
+                    if (rows[i + 4][2] !== 'D' && rows[i + 4][2] !== 'N') {
+                        return rows[i + 4].slice(2, 10);
+                    } else {
+                        return rows[i + 4].slice(2, 4);
+                    }
+                })(),
 
-            internationalScore: (() => {
-                if (rows[i + 4][2] !== 'D' && rows[i + 4][2] !== 'N') {
-                    return rows[i + 4].slice(10);
-                } else {
-                    return '0';
+                internationalScore: (() => {
+                    if (rows[i + 4][2] !== 'D' && rows[i + 4][2] !== 'N') {
+                        return rows[i + 4].slice(10);
+                    } else {
+                        return '0';
+                    }
                 }
-            }
-            )()
-        });
+                )()
+            });
+        }
+        console.log(parsedResults);
+        return parsedResults;
+    } catch (err) {
+        console.log({ message: err });
     }
-    console.log(parsedResults);
-    return parsedResults;
 }
 
 function reverseString(str) {
